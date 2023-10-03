60af8557e5f39fd9bbf7959cd3808ebac592b6e7
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-10-03 17:51:07 +0330

    add biography actor and more details

--- src/components/rightbox/ShowActorDetail.jsx
+++ src/components/rightbox/ShowActorDetail.jsx
@@ -4,7 +4,31 @@ import styles from "../../assets/styles/rightbox/ShowActor.module.scss";
 // eslint-disable-next-line react/prop-types
 function ShowActorDetail({ actorId }) {
   const [actor, setActor] = useState({});
+  const [deepActor, setDeepActor] = useState({});
+  const sendReqIdAcotr = (person_id) => {
+    const options = {
+      method: "GET",
+      url: `https://api.themoviedb.org/3/person/${person_id}`,
+      params: { include_adult: "false", language: "en-US", page: "1" },
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDE3MTk4NDI4ZDkxZGZiYThlNWU1YTQ1OWU1Mjc1MiIsInN1YiI6IjY1MTkzMmYxYTE5OWE2MDBlMWZjN2JlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qjZkw5ryAz3bt9Jf-TRCmW947WKGwgTAze3TrsfGDRU",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setDeepActor(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
   useEffect(() => {
+    setDeepActor({});
     const options = {
       method: "GET",
       url: "https://api.themoviedb.org/3/search/person",
@@ -20,6 +44,7 @@ function ShowActorDetail({ actorId }) {
       .request(options)
       .then(function (response) {
         setActor(response.data.results[0]);
+        sendReqIdAcotr(response.data.results[0].id);
       })
       .catch(function (error) {
         console.error(error);
@@ -27,18 +52,43 @@ function ShowActorDetail({ actorId }) {
   }, [actorId]);
   return (
     <div className={styles.container}>
-      {actor.id == undefined ? (
+      {deepActor.id == undefined ? (
         <p className={styles.loader}>Loading...</p>
       ) : (
         <>
           <div className={styles.containerBackimg}>
             <img className={styles.backImg} src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt="" />
           </div>
-          <div className={styles.actorDet}>
-            {console.log(actor)}
-            <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt="" />
-            <div className={styles.actorNameBorn}>
-              <p>{actor.name}</p>
+          <div className={styles.actorBox}>
+            <div className={styles.actorDet}>
+              {console.log(actor)}
+              <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt="" />
+              <div className={styles.actorNameBorn}>
+                <p>{actor.name}</p>
+                <p>{`${new Date().getFullYear() - deepActor.birthday.split("-")[0]} Yo`}</p>
+                <p>{deepActor.place_of_birth}</p>
+              </div>
+            </div>
+            <div className={styles.biography}>
+              <h2>Biography</h2>
+              <p>{deepActor.biography}</p>
+            </div>
+            <div className={styles.knownby}>
+              <h2>Known by</h2>
+              <div className={styles.knownmovies}>
+                <div>
+                  <img src={`https://image.tmdb1.org/t/p/w500${actor.profile_path}`} alt="known1" />
+                  <p>movie1</p>
+                </div>
+                <div>
+                  <img src={`https://image.tmdb1.org/t/p/w500${actor.profile_path}`} alt="known1" />
+                  <p>movie2</p>
+                </div>
+                <div>
+                  <img src={`https://image.tmdb1.org/t/p/w500${actor.profile_path}`} alt="known1" />
+                  <p>movie3</p>
+                </div>
+              </div>
             </div>
           </div>
         </>
